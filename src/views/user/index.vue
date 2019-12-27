733a3a375caeb7d97b64447476a3d5ad615fe61b
yucheng <yucheng.zhu@foxmail.com>
2019-12-27 21:52:40 +0800

    调整用户页面

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -56,7 +56,7 @@
           <span>{{ scope.row.roles.map(r => r.name).join(',') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="状态" width="70">
+      <el-table-column label="禁用" width="70">
         <template slot-scope="scope">
           <el-switch v-model="scope.row.disabled" @change="checkDisabledStatus(scope.row)"/>
         </template>
@@ -86,16 +86,26 @@
           <el-input v-model="temp.username" autocomplete="off"/>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="temp.password" type="password" autocomplete="off"/>
+          <el-input v-model="temp.password" type="password" autocomplete="off" show-password/>
         </el-form-item>
         <el-form-item label="姓名" prop="name">
           <el-input v-model="temp.name"/>
         </el-form-item>
-        <el-form-item label="电话">
+        <el-form-item label="角色" prop="roles">
+          <el-select v-model="temp.roles" multiple placeholder="请选择" style="width: 100%">
+            <el-option
+              v-for="item in allRoles"
+              :key="item.id"
+              :label="item.name"
+              :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
           <el-input v-model="temp.mobile"/>
         </el-form-item>
         <el-form-item label="生日">
-          <el-date-picker v-model="temp.birthday" type="date" placeholder="请选择生日"/>
+          <el-date-picker v-model="temp.birthday" type="date" placeholder="请选择生日" style="width: 100%;"/>
         </el-form-item>
         <el-form-item label="备注">
           <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="请输入备注信息"/>
@@ -121,6 +131,16 @@
         </el-form-item>
         <el-form-item label="姓名" prop="name">
           <el-input v-model="temp.name"/>
+        </el-form-item>
+        <el-form-item label="角色" prop="roles">
+          <el-select v-model="temp.roles" multiple placeholder="请选择" style="width: 100%">
+            <el-option
+              v-for="item in allRoles"
+              :key="item.id"
+              :label="item.name"
+              :value="item">
+            </el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="电话" prop="mobile">
           <el-input v-model="temp.mobile"/>
@@ -174,6 +194,7 @@
           q: undefined
         },
         temp: {},
+        allRoles: [],
         addFormVisible: false,
         editFormVisible: false,
         rules: {
@@ -188,14 +209,33 @@
           ],
           mobile: [
             { required: true, message: '电话不能为空', trigger: 'blur' },
+          ],
+          roles: [
+            { required: true, message: '角色不能为空', trigger: 'blur' }
           ]
         }
       }
     },
     created() {
       this.getList()
+      this.getAllRoles()
     },
     methods: {
+      getAllRoles() {
+        getRoles().then(response => {
+          this.allRoles = response.data.list
+        })
+      },
+      resetTemp() {
+        this.temp = {
+          roles: []
+        }
+      },
+      clearTemp() {
+        this.resetTemp()
+        this.addFormVisible = false
+        this.editFormVisible = false
+      },
       getList() {
         this.listLoading = true
         getUsers(this.listQuery).then(response => {
@@ -205,39 +245,33 @@
         })
       },
       handleCreate() {
+        this.resetTemp()
         this.addFormVisible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].clearValidate()
-        })
-      },
-      clearTemp() {
-        this.temp = {}
-        this.addFormVisible = false
-        this.editFormVisible = false
       },
       createData() {
-        this.$refs['dataForm'].validate((valid) => {
+        this.$refs['addForm'].validate((valid) => {
           if (valid) {
+            this.temp.disabled = false
             addUser(this.temp).then(response => {
-              this.getList()
-              this.addFormVisible = false
-              this.temp = {}
-              this.$notify({
+              this.$nextTick(() => {
+                this.getList()
+                this.clearTemp()
+              })
+              this.$notify.success({
                 title: '提示',
-                message: '创建成功',
-                type: 'success',
+                message: '创建成功'
               })
             })
           }
         })
       },
       checkDisabledStatus(row) {
-        let msg = !row.disabled ? '是否禁用该用户' : '是否启用该用户'
+        let msg = (!row.disabled) ? '是否启用该用户' : '是否禁用该用户'
         this.$confirm(msg, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
-        }).then(response => {
+        }).then(r => {
           updateUser({
             id: row.id,
             disabled: row.disabled
@@ -248,9 +282,7 @@
               type: 'success'
             })
           })
-        }).catch(() => {
-          row.disabled = !row.disabled
-        })
+        }).catch(() => row.disabled = !row.disabled)
       },
       handleUpdate(row) {
         getUserById(row.id).then(response => {
@@ -266,12 +298,10 @@
             this.temp.password = undefined
             updateUser(this.temp).then(response => {
               this.getList()
-              this.editFormVisible = false
-              this.temp = {}
-              this.$notify({
+              this.clearTemp()
+              this.$notify.success({
                 title: '提示',
-                message: '更新成功',
-                type: 'success',
+                message: '更新成功'
               })
             })
           }
@@ -279,18 +309,20 @@
       },
       handleDelete(row) {
         this.temp = Object.assign({}, row) // copy obj
-        this.deleteConfirmVisible = true
+        this.$confirm('是否删除该用户?', '警告', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(r => {
+          this.doDelete()
+        }).catch(() => {})
       },
       doDelete() {
-        const tempData = Object.assign({}, this.temp)
-        deleteUser(tempData.id).then(response => {
+        deleteUser(this.temp.id).then(response => {
           this.getList()
-          this.deleteConfirmVisible = false
-          this.$notify({
+          this.$notify.success({
             title: '提示',
-            message: '删除成功',
-            type: 'success',
-            duration: 2000
+            message: '删除成功'
           })
         })
       }
